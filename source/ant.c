268ba77bfcb0978f9cf9d24549a31b1c322cee15
Andrew Trotman <andrew@cs.otago.ac.nz>
2009-01-22 23:02:05 +0000

    using INEX assessments again
    git-svn-id: https://groke.otago.ac.nz/ant/ant@96 7489a7b4-4588-4892-8acd-774bacfab51c

--- source/ant.c
+++ source/ant.c
@@ -324,7 +324,7 @@ double map;
 if (argc == 1)
 	command_driven_ant();
 else if (argc == 3)
-	map = batch_ant(argv[1], argv[2], QREL_ANT);
+	map = batch_ant(argv[1], argv[2], QREL_INEX);
 
 #ifdef NEVER
 /*
