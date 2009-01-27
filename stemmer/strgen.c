37a3c0464956ad61cf619ec33c004aa6abcc4361
vwood <vwood@7489a7b4-4588-4892-8acd-774bacfab51c>
2009-01-27 03:03:42 +0000

    Updated makefile, added first term to string gen.
    git-svn-id: https://groke.otago.ac.nz/ant/ant@110 7489a7b4-4588-4892-8acd-774bacfab51c

--- stemmer/strgen.c
+++ stemmer/strgen.c
@@ -3,21 +3,19 @@
 #include "strgen.h"
 #include "btree_iterator.h"
 
-char **strings;
-int string_count;
+char **strings = NULL;
+int string_count = NULL;
 
 void init_strgen(ANT_search_engine *search_engine) {
     char *term;
     int i;
     ANT_btree_iterator iterator(search_engine);
 
-    for (i = 0, iterator.first(NULL); iterator.next() != NULL; i++)
+    for (string_count = 0, iterator.first(NULL); iterator.next() != NULL; string_count++)
         ;
 
-    string_count = i;
     strings = (char **) malloc(sizeof(strings[0]) * string_count);
-
-    for (i = 0, iterator.first(NULL); term != NULL; term = iterator.next(), i++)
+    for (i = 0, term = iterator.first(NULL); term != NULL; term = iterator.next(), i++)
         strings[i] = strdup(term);
 }
 
