3623318ac73da5ef6f65d5c708b27618cc202d1f
Andrew Trotman <andrew@cs.otago.ac.nz>
2009-03-18 00:29:36 +0000

    git-svn-id: https://groke.otago.ac.nz/ant/ant@159 7489a7b4-4588-4892-8acd-774bacfab51c

---
+++ source/maths.h
@@ -0,0 +1,37 @@
+/*
+	MATHS.H
+	-------
+*/
+
+#ifndef __MATHS_H__
+#define __MATHS_H__
+#ifdef _MSC_VER
+	#include <crtdefs.h>
+#else
+	
+#endif
+
+
+inline int sign(int a)
+{
+return a < 0 ? -1 : a > 0 ? 1 : 0;
+}
+
+inline int sign(long a)
+{
+return a < 0 ? -1 : a > 0 ? 1 : 0;
+}
+
+inline int sign(size_t a)
+{
+return a < 0 ? -1 : a > 0 ? 1 : 0;
+}
+
+inline int sign(ptrdiff_t a)
+{
+return a < 0 ? -1 : a > 0 ? 1 : 0;
+}
+
+
+
+#endif __MATHS_H__
