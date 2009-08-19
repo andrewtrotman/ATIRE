1986a3cf6caacd9966a8509fb8d3d269d11fd293
Andrew Trotman <andrew@cs.otago.ac.nz>
2009-08-19 12:11:06 +0000

    one parser for ASCII and UTF8 with Chinese Segmentation (kinda sorta)
    git-svn-id: https://groke.otago.ac.nz/ant/ant@444 7489a7b4-4588-4892-8acd-774bacfab51c

--- source/parser.c
+++ source/parser.c
@@ -3,6 +3,7 @@
 	--------
 */
 #include "parser.h"
+#include "plugin_manager.h"
 
 /*
 	ANT_PARSER::ANT_PARSER()
@@ -11,6 +12,10 @@
 ANT_parser::ANT_parser()
 {
 set_document(NULL);
+
+#ifdef ONE_PARSER
+	segmentation = NULL;
+#endif
 }
 
 /*
@@ -30,18 +35,62 @@ void ANT_parser::set_document(unsigned char *document)
 this->document = current = document;
 }
 
+#ifdef ONE_PARSER
+	/*
+		ANT_PARSER::SEGMENT()
+		---------------------
+	*/
+	void ANT_parser::segment(void)
+	{
+	segmentation = (unsigned char *)ANT_plugin_manager::instance().do_segmentation((unsigned char *)current_token.start, current_token.string_length);
+	}
+#endif
+
 /*
 	ANT_PARSER::GET_NEXT_TOKEN()
 	----------------------------
 */
 ANT_string_pair *ANT_parser::get_next_token(void)
 {
-unsigned char *start;
+unsigned char *start, *here;
 
+#ifdef ONE_PARSER
+	if (segmentation != NULL)
+		{
+		/*
+			The segmenter has returned a space seperated list of tokens and we now have to work through them.
+			My reading of Darren's Chinese Segmentation code is that each term is followed by a space and
+			the final space is followed by a '\0'.  That is, the last term has a space after it.
+		*/
+		here = segmentation;
+		while (ANT_isspace(*here))
+			here++;
+		start = here++;
+		while (!ANT_isspace(*here))
+			here++;
+
+		/*
+			Prepare for the next token (next call to get_next_token())
+		*/
+		if (*here == '\0')
+			segmentation = NULL;		// no more tokens in this segmentation
+		else
+			segmentation = here;		// the next token continues on from here
+
+		/*
+			Build the token and return it
+		*/
+		current_token.start = (char *)start;
+		current_token.string_length = here - start;
+
+		return &current_token;
+		}
+
+#endif
 while (!isheadchar(*current))
 	current++;
 
-if (ANT_isalpha(*current))				// alphabetic strings
+if (ANT_isalpha(*current))				// alphabetic strings (in the ASCII CodePage)
 	{
 	*current = ANT_tolower(*current);
 	start = current++;
@@ -54,7 +103,7 @@ if (ANT_isalpha(*current))				// alphabetic strings
 	current_token.start = (char *)start;
 	current_token.string_length = current - start;
 	}
-else if (ANT_isdigit(*current))				// numbers
+else if (ANT_isdigit(*current))				// numbers (in the ASCII CodePage)
 	{
 	start = current++;
 	while (ANT_isdigit(*current))
@@ -66,8 +115,21 @@ else if (ANT_isdigit(*current))				// numbers
 else if (*current == '\0')						// end of string
 	return NULL;
 #ifdef ONE_PARSER
-	else if ((*current & 0x80) && ischinese(current))
+	else if (*current & 0x80)		// UTF-8 character
 		{
+		if (ischinese(current))		// Chinese CodePage
+			{
+			start = current;
+			while (ischinese(current))		// don't need to check for '\0' because that isn't a Chinese character
+				current += utf8_bytes(current);
+			/*
+				At this point we have the start and end of a sequence of chinese characters
+				which then needs to be segmented.
+			*/
+			current_token.start = (char *)start;
+			current_token.string_length = current - start;
+			segment();
+			}
 		}
 #endif
 else											// everything else (that starts with a '<')
--- source/parser.h
+++ source/parser.h
@@ -17,6 +17,9 @@ protected:
 	unsigned char *document;
 	unsigned char *current;
 	ANT_string_pair current_token;
+#ifdef ONE_PARSER
+	unsigned char *segmentation;
+#endif
 
 protected:
 	static int isheadchar(unsigned char val) { return ANT_isalnum(val) || val == '<' || val == '\0'; }
@@ -29,8 +32,10 @@ public:
 	static int isXMLnamechar(unsigned char val) { return isXMLnamestartchar(val) || ANT_isdigit(val) || val == '.' || val == '-'; } // see http://www.w3.org/TR/REC-xml/#NT-NameChar
 
 #ifdef ONE_PARSER
+	virtual void segment(void);
 	static int ischinese(unsigned char *here);
 	static unsigned long utf8_to_wide(unsigned char *here);
+	static long utf8_bytes(unsigned char *here);
 #endif
 
 	virtual void set_document(unsigned char *document);
@@ -38,9 +43,11 @@ public:
 } ;
 
 #ifdef ONE_PARSER
+
 	/*
 		ANT_PARSER::UTF8_TO_WIDE()
 		--------------------------
+		Convert a UTF8 sequence into a wide character
 	*/
 	inline unsigned long ANT_parser::utf8_to_wide(unsigned char *here)
 	{
@@ -58,6 +65,7 @@ public:
 	/*
 		ANT_PARSER::ISCHINESE()
 		-----------------------
+		Is the given character from the Chinese CodePoint?
 	*/
 	inline int ANT_parser::ischinese(unsigned char *here)
 	{
@@ -69,6 +77,25 @@ public:
 		|| (cp >=0xf900 && cp <= 0xfaff)
 		|| (cp >=0x2f800 && cp <= 0x2fa1f));
 	}
+
+	/*
+		ANT_PARSER::UTF8_BYTES()
+		------------------------
+		How many bytes does the UTF8 character take?
+	*/
+	inline long ANT_parser::utf8_bytes(unsigned char *here)
+	{
+	if (*here < 0x80)				// 1-byte (ASCII) character
+		return 1;
+	if ((*here & 0xE0) == 0xC0)		// 2-byte sequence
+		return 2;
+	if ((*here & 0xF0) == 0xE0)		// 3-byte sequence
+		return 3;
+	if ((*here & 0xF8) == 0xF0)		// 4-byte sequence
+		return 4;
+	return 1;		// dunno so make it 1
+	}
+
 #endif
 
 #endif __PARSER_H__
