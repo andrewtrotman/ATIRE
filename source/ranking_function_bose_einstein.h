6484eb9ca06ade5834008e60e454a9423d6c6df4
Andrew Trotman <andrew@cs.otago.ac.nz>
2009-10-07 06:12:31 +0000

    formatting change
    git-svn-id: https://groke.otago.ac.nz/ant/ant@500 7489a7b4-4588-4892-8acd-774bacfab51c

--- source/ranking_function_bose_einstein.h
+++ source/ranking_function_bose_einstein.h
@@ -14,7 +14,7 @@
 class ANT_ranking_function_bose_einstein : public ANT_ranking_function
 {
 public:
-	ANT_ranking_function_bose_einstein(ANT_search_engine *engine) : ANT_ranking_function(engine) { }
+	ANT_ranking_function_bose_einstein(ANT_search_engine *engine) : ANT_ranking_function(engine) {}
 	virtual ~ANT_ranking_function_bose_einstein() {}
 
 	virtual void relevance_rank_top_k(ANT_search_engine_accumulator *accumulator, ANT_search_engine_btree_leaf *term_details, ANT_compressable_integer *impact_ordering, long long trim_point);
