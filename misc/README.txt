c36449664f3fc507e033339cce680f26b75d6bb7
etang <etang@7489a7b4-4588-4892-8acd-774bacfab51c>
2009-12-14 05:44:18 +0000

    added support for another kind TREC id format
    git-svn-id: https://groke.otago.ac.nz/ant/ant@624 7489a7b4-4588-4892-8acd-774bacfab51c

--- misc/README.txt
+++ misc/README.txt
@@ -8,3 +8,5 @@
 
 
 1. ant_style.xml is the ECLIPSE ANT code style file
+
+2. ANT_coding_convention.txt is the 10 rules of ANT coding convention
--- source/directory_iterator_file.c
+++ source/directory_iterator_file.c
@@ -24,19 +24,32 @@ return next(object, get_file);
 */
 ANT_directory_iterator_object *ANT_directory_iterator_file::next(ANT_directory_iterator_object *object, long get_file)
 {
-char *document_id_start, *document_id_end;
+char *document_id_start = NULL, *document_id_end = NULL;
 
 if ((document_start = strstr(document_end, "<DOC")) != NULL)
+	{
 	if ((document_id_start = strstr(document_start, "<DOCNO>")) != NULL)
-		if ((document_id_end = strstr(document_id_start += 7, "</DOCNO>")) != NULL)
-			if ((document_end = strstr(document_id_end, "</DOC>")) != NULL)
-				{
-				strncpy(object->filename, document_id_start, document_id_end - document_id_start);
-				object->filename[document_id_end - document_id_start] = '\0';
-				if (get_file)
-					read_entire_file(object);
-				return object;
-				}
+		{
+		document_id_end = strstr(document_id_start += 7, "</DOCNO>");
+		if (document_id_end != NULL)
+			document_start = document_id_end + 8;
+		}
+	else if ((document_id_start = strstr(document_start, "id=\"")) != NULL)
+		{
+		document_id_end = strchr(document_id_start += 5, '"');
+		if (document_id_end)
+			document_start = strchr(document_id_end, '>') + 1;
+		}
+
+	if (document_id_end != NULL && (document_end = strstr(document_id_end, "</DOC>")) != NULL)
+		{
+		strncpy(object->filename, document_id_start, document_id_end - document_id_start);
+		object->filename[document_id_end - document_id_start] = '\0';
+		if (get_file)
+			read_entire_file(object);
+		return object;
+		}
+	}
 return NULL;
 }
 
