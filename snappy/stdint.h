a15bace0c62edeb3fad1cba3b8b0382908424f6c
Andrew Trotman <andrew@cs.otago.ac.nz>
2011-09-20 01:15:29 +0000

    working on the VC 2008 32 bit and VC 2010 64 bit build both working - hopefully it works now.
    git-svn-id: https://groke.otago.ac.nz/ant/ant@1229 7489a7b4-4588-4892-8acd-774bacfab51c

--- snappy/stdint.h
+++ snappy/stdint.h
@@ -1,12 +1,12 @@
 /*
 	STDINT.H
 	--------
-	Microsoft Visual Studio 2008 doesn't have stdint,h so this acts as a substitute
+	Microsoft Visual Studio 2008 doesn't have stdint,h so this acts as a stub
 */
 #ifndef ANT_STDINT_H_
 #define ANT_STDINT_H_
 
-#if _MSC_VER <= 1500
+#ifdef _MSC_VER
 
 	#include "../source/fundamental_types.h"
 
