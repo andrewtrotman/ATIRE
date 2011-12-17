3c4d95f480fcb8fd0c7c0b93fb9bab17ab894250
fei <fei@7489a7b4-4588-4892-8acd-774bacfab51c>
2011-12-17 01:19:53 +0000

    forgot to commit the impact header file
    git-svn-id: https://groke.otago.ac.nz/ant/ant@1322 7489a7b4-4588-4892-8acd-774bacfab51c

---
+++ source/impact_header.h
@@ -0,0 +1,29 @@
+/*
+	IMPACT_HEADER.H
+	--------------------------
+*/
+#ifndef IMPACT_HEADER_H_
+#define IMPACT_HEADER_H_
+
+#include "compress.h"
+#include <stdint.h>
+
+/*
+	class ANT_IMPACT_HEADER
+	----------------------------------
+*/
+class ANT_impact_header
+{
+public:
+	static const uint32_t NUM_OF_QUANTUMS = 256;
+	long long info_size, header_size;
+	ANT_compressable_integer *header_buffer;
+	uint32_t the_quantum_count;
+	uint32_t beginning_of_the_postings;
+	ANT_compressable_integer *impact_value_start, *impact_value_ptr;
+	ANT_compressable_integer *doc_count_start, *doc_count_ptr, *doc_count_trim_ptr;
+	ANT_compressable_integer *impact_offset_start, *impact_offset_ptr;
+};
+
+#endif  /* IMPACT_HEADER_H_ */
+
