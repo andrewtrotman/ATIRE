9da4dfe8d4bc808838c03d93e87ab71deb442277
Andrew Trotman <andrew@cs.otago.ac.nz>
2012-01-25 01:35:45 +0000

    removal of GPL lovins stemmer and replacement by BSD Snowball versions (that's better anyway).
    git-svn-id: https://groke.otago.ac.nz/ant/ant@1373 7489a7b4-4588-4892-8acd-774bacfab51c

--- atire/indexer_param_block_stem.c
+++ atire/indexer_param_block_stem.c
@@ -32,15 +32,12 @@ stemmer_similarity_threshold = 0.0;
 */
 void ANT_indexer_param_block_stem::help(long has_cutoff)
 {
-char paice_husk[2], lovins[2], snowball[2];
+char paice_husk[2], snowball[2];
 
-paice_husk[0] = paice_husk[1] = lovins[0] = lovins[1] = snowball[0] = snowball[1] = '\0';
+paice_husk[0] = paice_husk[1] = snowball[0] = snowball[1] = '\0';
 
 #ifdef ANT_HAS_PAICE_HUSK
 	paice_husk[0] = 'h';
-#endif
-#ifdef ANT_HAS_LOVINS
-	lovins[0] = 'l';
 #endif
 #ifdef ANT_HAS_SNOWBALL
 	snowball[0] = 'X';
@@ -48,7 +45,7 @@ paice_husk[0] = paice_husk[1] = lovins[0] = lovins[1] = snowball[0] = snowball[1
 
 puts("TERM EXPANSION");
 puts("--------------");
-printf("-t[-D%sk%soOpsS%s]", paice_husk, lovins, snowball);
+printf("-t[-D%skoOpsS%s]", paice_husk, snowball);
 
 if (has_cutoff)
 	printf("[+-<th>] ");
@@ -59,16 +56,13 @@ printf("Term expansion, one of:\n");
 puts("  -             None [default]");
 puts("  D             Double Metaphone phonetics");
 #ifdef ANT_HAS_PAICE_HUSK
-puts("  h             Paice Husk stemming");
+puts("  h             English Paice Husk stemming");
 #endif
-puts("  k             Krovetz stemming");
-#ifdef ANT_HAS_LOVINS
-puts("  l             Lovins stemming");
-#endif
-puts("  o             Otago stemming");
-puts("  O             Otago stemming version 2");
-puts("  p             Porter stemming (ATIRE's version of Porter v1)");
-puts("  s             S-Striping stemming");
+puts("  k             English Krovetz stemming");
+puts("  o             English Otago stemming");
+puts("  O             English Otago stemming version 2");
+puts("  p             English Porter stemming (ATIRE's version of Porter v1)");
+puts("  s             English S-Striping stemming");
 puts("  S             Soundex phonetics");
 #ifdef ANT_HAS_SNOWBALL
 	puts("  X[abcDdEeFfg2hiklnpRrSst] Stemming using the Snowball libraries");
@@ -122,9 +116,6 @@ switch (*which)
 	case 'h' : stemmer = ANT_stemmer_factory::PAICE_HUSK; break;
 #endif
 	case 'k' : stemmer = ANT_stemmer_factory::KROVETZ;    break;
-#ifdef ANT_HAS_LOVINS
-	case 'l' : stemmer = ANT_stemmer_factory::LOVINS;     break;
-#endif
 	case 'p' : stemmer = ANT_stemmer_factory::PORTER;     break;
 	case 'o' : stemmer = ANT_stemmer_factory::OTAGO;      break;
 	case 'O' : stemmer = ANT_stemmer_factory::OTAGO_V2;   break;
@@ -132,7 +123,10 @@ switch (*which)
 	case 'D' : stemmer = ANT_stemmer_factory::DOUBLE_METAPHONE;       break;
 	case 'S' : stemmer = ANT_stemmer_factory::SOUNDEX; break;
 	case 'X' :
-		switch(*(which + 1))
+		if ((*which + 2) != '\0')
+			exit(printf("Unknown stemmer: '-%s'\n", *which));
+
+		switch (*(which + 1))
 			{
 			case 'a': stemmer = ANT_stemmer_factory::SNOWBALL_ARMENIAN; break;
 			case 'b': stemmer = ANT_stemmer_factory::SNOWBALL_BASQUE; break;
