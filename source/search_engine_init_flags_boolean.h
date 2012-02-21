3bac27c0ce15ab287c82c25ac9fa2ecca727e3b8
Andrew Trotman <andrew@cs.otago.ac.nz>
2012-02-21 09:06:01 +0000

    removed diagnostics and cleaned the code a bit
    git-svn-id: https://groke.otago.ac.nz/ant/ant@1434 7489a7b4-4588-4892-8acd-774bacfab51c

--- source/search_engine_init_flags_boolean.h
+++ source/search_engine_init_flags_boolean.h
@@ -5,7 +5,6 @@
 #ifndef SEARCH_ENGINE_INIT_FLAGS_BOOLEAN_H_
 #define SEARCH_ENGINE_INIT_FLAGS_BOOLEAN_H_
 
-#include <stdio.h>			// for puts()
 #include <string.h>
 #include "bitstring.h"
 
@@ -22,35 +21,40 @@ public:
 	ANT_search_engine_init_flags_boolean() : ANT_bitstring() { }
 	~ANT_search_engine_init_flags_boolean() { }
 
-	void init(long long documents)
-		{
-		ANT_bitstring::set_length(initial_length = length = documents);
-		rewind();
-		}
-
-	void set_length(long long new_length)
-		{
-		if (new_length > initial_length)
-			puts("bitstring initial length exceeded");
-
-		length = new_length;
-		ANT_bitstring::unsafe_set_length(new_length);
-		}
-
-	void resize(long long new_length)
-		{
-		if (new_length > initial_length)
-			printf("bitstring initial length exceeded (%lld > %lld)", new_length, initial_length);
-
-		length = new_length;
-		ANT_bitstring::unsafe_set_length(new_length);
-		}
-
+	void init(long long documents);
+	void set_length(long long new_length);
+	void resize(long long new_length) { set_length(new_length); }
 	void rewind(void) { ANT_bitstring::zero(); }
 
 	inline long get(long long index) { return ANT_bitstring::unsafe_getbit(index); }
 	inline void set(long long index) { ANT_bitstring::unsafe_setbit(index); }
 	inline void unset(long long index) { ANT_bitstring::unsafe_unsetbit(index); }
 } ;
+
+
+/*
+	ANT_SEARCH_ENGINE_INIT_FLAGS_BOOLEAN::INIT()
+	--------------------------------------------
+*/
+inline void ANT_search_engine_init_flags_boolean::init(long long documents)
+{
+ANT_bitstring::set_length(initial_length = length = documents);
+rewind();
+}
+
+/*
+	ANT_SEARCH_ENGINE_INIT_FLAGS_BOOLEAN::SET_LENGTH()
+	--------------------------------------------------
+*/
+inline void ANT_search_engine_init_flags_boolean::set_length(long long new_length)
+{
+if ((length = new_length) > initial_length)
+	{
+	initial_length = length;
+	ANT_bitstring::set_length(new_length);
+	}
+else
+	ANT_bitstring::unsafe_set_length(new_length);
+}
 
 #endif /* SEARCH_ENGINE_INIT_FLAGS_BOOLEAN_H_ */
