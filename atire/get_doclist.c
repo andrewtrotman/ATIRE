c999c9716693b3dc75590eef278544ec7fa118cf
mcrane <mcrane@7489a7b4-4588-4892-8acd-774bacfab51c>
2012-02-27 09:18:35 +0000

    filled in the missing pieces to get_doclist
    git-svn-id: https://groke.otago.ac.nz/ant/ant@1452 7489a7b4-4588-4892-8acd-774bacfab51c

--- atire/get_doclist.c
+++ atire/get_doclist.c
@@ -2,18 +2,10 @@
 	GET_DOCLIST.C
 	-------------
 */
+#include "../source/file.h"
 #include "../source/memory.h"
 #include "../source/search_engine.h"
-#include "../source/search_engine_btree_leaf.h"
-
-/*
-	GET_VARIABLE()
-	--------------
-*/
-long long get_variable(ANT_search_engine *engine, char *name)
-{
-return engine->get_variable(name);
-}
+#include "../source/str.h"
 
 /*
 	MAIN()
@@ -23,11 +15,37 @@ int main(void)
 {
 ANT_memory memory;
 ANT_search_engine search_engine(&memory);
-long long start, end;
+ANT_file doclist;
+unsigned long buffer_length;
+long long start, end, doc;
+char *buffer;
+char **filenames;
 
-start = get_variable(&search_engine, "~documentfilenamesstart");
-end = get_variable(&search_engine, "~documentfilenamesfinish");
-printf("start:%lld end:%lld\n", start, end);
+search_engine.open();
+
+/*
+	Don't overwrite doclist.aspt if it exists, which we check by opening read-only first
+*/
+if (doclist.open("doclist.aspt", "r"))
+	exit(printf("doclist.aspt already exists\n"));
+doclist.close();
+doclist.open("doclist.aspt", "w");
+
+start = search_engine.get_variable("~documentfilenamesstart");
+end = search_engine.get_variable("~documentfilenamesfinish");
+
+if (start == 0 || end == 0)
+	exit(printf("Filenames aren't in the index\n"));
+
+buffer_length = (unsigned long)(end - start);
+buffer = (char *)malloc(sizeof(*buffer) * buffer_length);
+
+filenames = search_engine.get_document_filenames(buffer, &buffer_length);
+
+for (doc = 0; doc < search_engine.document_count(); doc++)
+	doclist.puts(strip_space_inplace(filenames[doc]));
+
+doclist.close();
 
 return 0;
 }
