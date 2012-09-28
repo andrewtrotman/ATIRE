013c0467e2d9cbf44048b5ecef000680bd09f50c
Xiangfei Jia <fei@cs.otago.ac.nz>
2012-09-28 15:50:05 +1200

    added the skeleton code for the impact header functions for the new ranking functions

--- source/ranking_function_dfiw_idf.h
+++ source/ranking_function_dfiw_idf.h
@@ -18,7 +18,12 @@ public:
 	ANT_ranking_function_DFIW_IDF(long long documents, ANT_compressable_integer *document_lengths) : ANT_ranking_function(documents, document_lengths) {}
 	virtual ~ANT_ranking_function_DFIW_IDF() {}
 
+#ifdef IMPACT_HEADER
+	virtual void relevance_rank_one_quantum(ANT_ranking_function_quantum_parameters *quantum_parameters);
+	virtual void relevance_rank_top_k(ANT_search_engine_result *accumulator, ANT_search_engine_btree_leaf *term_details, ANT_impact_header *impact_header, ANT_compressable_integer *impact_ordering, long long trim_point, double prescalar, double postscalar);
+#else
 	virtual void relevance_rank_top_k(ANT_search_engine_result *accumulator, ANT_search_engine_btree_leaf *term_details, ANT_compressable_integer *impact_ordering, long long trim_point, double prescalar, double postscalar);
+#endif
 	virtual double rank(ANT_compressable_integer docid, ANT_compressable_integer length, unsigned char term_frequency, long long collection_frequency, long long document_frequency);
 } ;
 
