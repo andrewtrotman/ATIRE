d34bca1cdffd45287cf95f219f855c8ae9f0df7a
Fei <fei@cs.otago.ac.nz>
2013-07-13 19:17:39 +1200

    minor change

--- experiments/quantum-at-a-time/run.sh
+++ experiments/quantum-at-a-time/run.sh
@@ -34,15 +34,15 @@ if [ "$Q_OR_T" = "QaaT" ]; then
 elif [ "$Q_OR_T" = "TaaT" ]; then
 	strategies=( M )
 fi
+
 #
 # run the benchmark for the small range of top k
 #
-
 for st in "${strategies[@]}"
 do
 	cmd="python ${py_script} --ps ${st} -a ${assessment_file} -q ${query_file} --outfile output-small-${s}.csv --range small"
 	echo $cmd
-	#$cmd
+	$cmd
 done
 
 #
@@ -52,5 +52,5 @@ for st in "${strategies[@]}"
 do
 	cmd="python ${py_script} --ps ${st} -a ${assessment_file} -q ${query_file} --outfile output-large-${s}.csv --range large"
 	echo $cmd
-	#$cmd
+	$cmd
 done
