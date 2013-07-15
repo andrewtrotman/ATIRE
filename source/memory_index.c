30887c1bc5289b835583527c497a2d4dfd803733
Matt Crane <mcrane@cs.otago.ac.nz>
2013-07-15 14:24:45 +1200

    Oversight with quantization

--- source/memory_index.c
+++ source/memory_index.c
@@ -1152,7 +1152,8 @@ if ((quantizer = factory->get_indexing_ranker(largest_docno, document_lengths, &
 	set_variable("~quantmax", *(long long *)&maximum_collection_rsv);
 	set_variable("~quantmin", *(long long *)&minimum_collection_rsv);
 	}
-else
+
+if (quantizer == NULL || !index_quantization)
 	quantization_bits = 8; // this is by default for tf-indexes
 
 /*
