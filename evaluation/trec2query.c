21c60cbf35e12a639c254a4ce656130500944555
Blake Burgess <bburgess@cs.otago.ac.nz>
2013-12-12 12:55:46 +1300

    Rewrote trec2query to use same parsing style as main ATIRE engine. Updated evaluation/GNUmakefile for dependencies; other evaluation/makefiles may need adjustments.

--- evaluation/trec2query.c
+++ evaluation/trec2query.c
@@ -4,94 +4,10 @@
 */
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
-#include <sstream>
-#include "stop_word.h"
+#include "../source/channel_file.h"
+#include "../source/channel_trec.h"
 
 using namespace std;
-
-char buffer[1024 * 1024];
-
-const char *new_stop_words[] =
-	{
-//	"alternative",
-	"arguments",
-	"can",
-	"current",
-	"dangers",
-	"data",
-	"description",
-	"developments",
-	"document",
-	"documents",
-	"done",
-	"discuss",
-	"discusses",
-	"efforts",
-	"enumerate",
-	"examples",
-	"help",
-	"ideas",
-	"identify",
-	"inform",
-	"information",
-	"instances",
-	"latest",
-	"method",
-	"narrative",
-	"occasions",
-	"problems",
-	"provide",
-	"relevant",
-	"report",
-	"reports",
-	"state",
-	"topic",
-	NULL
-	} ;
-
-#define FALSE 0
-#define TRUE (!FALSE)
-
-
-/*
-	STRIP_SPACE_INLINE()
-	--------------------
-*/
-char *strip_space_inline(char *source)
-{
-char *end, *start = source;
-
-while (isspace(*start))
-	start++;
-
-if (start > source)
-	memmove(source, start, strlen(start) + 1);		// copy the '\0'
-
-end = source + strlen(source) - 1;
-while ((end >= source) && (isspace(*end)))
-	*end-- = '\0';
-
-return source;
-}
-
-#ifndef _MSC_VER
-/*
-	STRLWR()
-	--------
-*/
-void strlwr(char *source)
-{
-while (*source)
-	{
-	*source = tolower(*source);
-	source++;
-	}
-}
-void strlwr(const char *source) { strlwr((char *)source); }
-#endif
-
 
 /*
 	MAIN()
@@ -99,93 +15,25 @@ void strlwr(const char *source) { strlwr((char *)source); }
 */
 int main(int argc, char *argv[])
 {
-static const char *SEPERATORS = ",./;'[]!@#$%^&*()_+-=\\|<>?:{}\r\n\t \"`~";
-ANT_stop_word stopper;
-FILE *fp;
-char *from, *ch, *tag;
-long number, old_number, match, read;
+ANT_channel *inchannel;
+ANT_channel *outchannel;
+char *inchannel_word;
 
 if (argc != 3)
-	exit(printf("Usage:%s <infile> <tag>\n<tag> is a combination of T,D,N (title, desc, narr)", argv[0]));
+  exit(printf("Usage:%s <trectopicfile> <tag>\n<tag> is any combination of T, D, N (title, desc, narr)", argv[0]));
 
-if ((fp = fopen(argv[1], "rb")) == NULL)
-	exit(printf("Can't open file :%s\n", argv[1]));
+inchannel = new ANT_channel_file(argv[1]);
+outchannel = new ANT_channel_file();
 
-tag = argv[2];
-strlwr(tag);
+inchannel = new ANT_channel_trec(inchannel, argv[2]);
+for (inchannel_word = inchannel->gets(); inchannel_word != NULL; inchannel_word = inchannel->gets())
+  {
+  outchannel->puts(inchannel_word);
+  }
 
-stopper.addstop((const char **)new_stop_words);
-old_number = number = -1;
+if (outchannel != inchannel)
+  delete outchannel;
+delete inchannel;
 
-read = TRUE;
-while (1)
-	{
-	if (read)
-		if (fgets(buffer, sizeof(buffer), fp) == NULL)
-			break;
-
-	read = TRUE;
-	if (strncmp(buffer, "<num>", 5) == 0)
-		number = atol(strchr(buffer, ':') + 1);
-	else
-		{
-		match = FALSE;
-
-		if (*buffer == '<')
-			{
-			if ((strncmp(buffer + 1, "title", 5) == 0) && (strchr(tag, 't') != NULL))
-				match = TRUE;
-			if ((strncmp(buffer + 1, "desc",  4) == 0) && (strchr(tag, 'd') != NULL))
-				match = TRUE;
-			if ((strncmp(buffer + 1, "narr",  4) == 0) && (strchr(tag, 'n') != NULL))
-				match = TRUE;
-			}
-		if (match)
-			{
-			ostringstream query;
-
-			query << strchr(buffer, '>') + 1;			// character after the "<desc>"
-			while (fgets(buffer, sizeof(buffer), fp) != NULL)
-				{
-				if (*buffer == '<')
-					{
-					read = FALSE;
-					break;
-					}
-				strip_space_inline(buffer);
-				query << ' ' << buffer;		// <-
-				}
-
-			query << '\0';
-			from = (char *)query.str().c_str();
-			strlwr(from);
-			for (ch = from; *ch != '\0'; ch++)
-				if (isspace(*ch))
-					*ch = ' ';
-
-			strip_space_inline(from);
-
-			if (number >= 0)
-				{
-				if (number != old_number)
-					{
-					printf("\n%ld", number);
-					old_number = number;
-					}
-				for (ch = strtok(from, SEPERATORS); ch != NULL; ch = strtok(NULL, SEPERATORS))
-					{
-					if (!stopper.isstop(ch))
-						if (strlen(ch) > 2)			// stop words 2 characters or less
-							if (!isdigit(*ch))		// doesn't contain a number
-								printf(" %s", ch);
-					}
-				}
-			else
-				exit(printf("\nBroken file\n"));
-			}
-		}
-	}
-printf("\n");
-fclose(fp);
-return 0;
+return EXIT_SUCCESS;
 }
