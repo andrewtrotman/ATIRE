50346eda5977ec118e8c4879619a99929cf46c47
Blake Burgess <bburgess@cs.otago.ac.nz>
2013-12-12 13:03:38 +1300

    Whitespace in trec2query

--- evaluation/trec2query.c
+++ evaluation/trec2query.c
@@ -1,6 +1,6 @@
 /*
-	TREC2QUERY.C
-	------------
+    TREC2QUERY.C
+    ------------
 */
 #include <stdio.h>
 #include <stdlib.h>
@@ -10,8 +10,8 @@
 using namespace std;
 
 /*
-	MAIN()
-	------
+    MAIN()
+    ------
 */
 int main(int argc, char *argv[])
 {
@@ -20,19 +20,20 @@ ANT_channel *outchannel;
 char *inchannel_word;
 
 if (argc != 3)
-  exit(printf("Usage:%s <trectopicfile> <tag>\n<tag> is any combination of T, D, N (title, desc, narr)", argv[0]));
+    exit(printf("Usage:%s <trectopicfile> <tag>\n<tag> is any combination of T, D, N (title, desc, narr)", argv[0]));
 
 inchannel = new ANT_channel_file(argv[1]);
 outchannel = new ANT_channel_file();
 
 inchannel = new ANT_channel_trec(inchannel, argv[2]);
 for (inchannel_word = inchannel->gets(); inchannel_word != NULL; inchannel_word = inchannel->gets())
-  {
-  outchannel->puts(inchannel_word);
-  }
+    {
+    outchannel->puts(inchannel_word);
+    }
 
 if (outchannel != inchannel)
-  delete outchannel;
+    delete outchannel;
+
 delete inchannel;
 
 return EXIT_SUCCESS;
