43c9ec8f0b2d44bd6502b5bb494b6fe724c448bf
Blake Burgess <bburgess@cs.otago.ac.nz>
2013-12-12 13:23:05 +1300

    Fixed evaluation/GNUmakefile to resolve bz2 dependency issue. Minor trec2query.

--- evaluation/trec2query.c
+++ evaluation/trec2query.c
@@ -15,25 +15,22 @@ using namespace std;
 */
 int main(int argc, char *argv[])
 {
-ANT_channel *inchannel;
-ANT_channel *outchannel;
+ANT_channel *inchannel, *outchannel;
 char *inchannel_word;
 
 if (argc != 3)
     exit(printf("Usage:%s <trectopicfile> <tag>\n<tag> is any combination of T, D, N (title, desc, narr)", argv[0]));
 
 inchannel = new ANT_channel_file(argv[1]);
-outchannel = new ANT_channel_file();
+outchannel = new ANT_channel_file(); // Defaults to stdout
 
 inchannel = new ANT_channel_trec(inchannel, argv[2]);
+
 for (inchannel_word = inchannel->gets(); inchannel_word != NULL; inchannel_word = inchannel->gets())
-    {
     outchannel->puts(inchannel_word);
-    }
 
 if (outchannel != inchannel)
     delete outchannel;
-
 delete inchannel;
 
 return EXIT_SUCCESS;
