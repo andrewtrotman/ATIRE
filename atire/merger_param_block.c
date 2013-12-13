330c982ef1db172a851f72ff4de91f77e0cf1cc0
Blake Burgess <bburgess@cs.otago.ac.nz>
2013-12-13 13:31:32 +1300

    atire_merge can now generate an index containing only the specified terms. terms must be one per line, sorted, ascending (aardvark first, zebra last). Forgot to add the qsort in this commit.

--- atire/merger_param_block.c
+++ atire/merger_param_block.c
@@ -32,6 +32,8 @@ this->argv = argv;
 reporting_frequency = 0;
 index_filename = "merged_index.aspt";
 doclist_filename = "merged_doclist.aspt";
+intersection_filename = "intersection_list";
+skip_intersection = true;
 document_compression_scheme = ANT_compression_text_factory::DEFLATE; // something non-NONE, doesn't matter what
 }
 
@@ -93,6 +95,7 @@ puts("");
 
 puts("OPTIMISATIONS");
 puts("-------------");
+puts("-I <fn>         Remove terms not in file fn, i.e. return intersection of (indexes) and (terms in fn)");
 puts("-K<n>           Static pruning. Write no more than <n> postings per list (0=all) [default=0]");
 puts("                Static pruning will be performed to the minimum of the parameter given");
 puts("                and the sum of static prune points in the indexes to be merged.");
@@ -157,6 +160,11 @@ for (param = 1; param < argc; param++)
 			index_filename = argv[++param];
 		else if (strcmp(command, "fdoclist") == 0)
 			doclist_filename = argv[++param];
+		else if (strcmp(command, "I") == 0)
+      {
+			intersection_filename = argv[++param];
+      skip_intersection = false;
+      }
 		else if (strcmp(command, "nologo") == 0)
 			logo = FALSE;
 		else
