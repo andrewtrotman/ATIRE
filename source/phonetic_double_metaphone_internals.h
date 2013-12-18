8cc203eb150fafa9510efdbff8ce1cfd77794fc9
Andrew Trotman <andrew@cs.otago.ac.nz>
2013-12-18 15:07:36 +1300

    remove more Linux warnings

--- source/phonetic_double_metaphone_internals.h
+++ source/phonetic_double_metaphone_internals.h
@@ -393,6 +393,8 @@ private:
 */
 #ifdef __APPLE__
 			case (tchar)0xC7: // 'Ç':		// character 0xC7 (decimal 199)
+#elif defined (__GNUC__)
+			case (tchar)0xC7: // 'Ç':		// character 0xC7 (decimal 199)
 #else
 			case 0xC7: // 'Ç':		// character 0xC7 (decimal 199)
 #endif
@@ -759,6 +761,8 @@ private:
 	ANT:This is the N tilde Character (Spanish ene)
 */
 #ifdef __APPLE__
+			case (tchar)0xD1: //'Ñ':		// 0xD1, (decimal 209)
+#elif defined (__GNUC__)
 			case (tchar)0xD1: //'Ñ':		// 0xD1, (decimal 209)
 #else
 			case 0xD1: //'Ñ':		// 0xD1, (decimal 209)
