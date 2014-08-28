50999a4350d422e5b954b9c7ceb6fb12edb334be
Andrew Trotman <andrew@cs.otago.ac.nz>
2014-08-29 11:06:47 +1200

    removed white space

--- source/ranking_function_puurula.c
+++ source/ranking_function_puurula.c
@@ -63,34 +63,7 @@ else
 */
 void ANT_ranking_function_puurula::relevance_rank_one_quantum(ANT_ranking_function_quantum_parameters *quantum_parameters)
 {
-long long docid;
-double rsv, tf, cf, query_length, query_occurences, prior;
-ANT_compressable_integer *current;
-
-query_length = quantum_parameters->accumulator->get_term_count();
-query_occurences = quantum_parameters->query_frequency;
-
-cf = (double)quantum_parameters->term_details->global_collection_frequency;
-
-tf = quantum_parameters->tf;
-tf = max(tf - g * pow(tf, g), 0);
-																									
-rsv = query_occurences * log((tf * quantum_parameters->prescalar * collection_length_in_terms) / (u * cf) + 1.0);
-
-docid = -1;
-current = quantum_parameters->the_quantum;
-while (current < quantum_parameters->quantum_end)
-	{
-	docid += *current++;
-
-	if (quantum_parameters->accumulator->is_zero_rsv(docid))		// unseen before now so add the document prior
-		{
- 		prior = log(1.0 - discounted_document_lengths[(size_t)docid] / ((double)document_lengths[(size_t)docid] + u));
-		quantum_parameters->accumulator->add_rsv(docid, quantize(quantum_parameters->postscalar * (query_length * prior + rsv), maximum_collection_rsv, minimum_collection_rsv));
-		}
-	else
-		quantum_parameters->accumulator->add_rsv(docid, quantize(quantum_parameters->postscalar * rsv, maximum_collection_rsv, minimum_collection_rsv));
-	}
+exit(printf("Cannot use lmp and quantum processing (at the moment)\n"));
 }
 
 /*
@@ -115,7 +88,7 @@ while (impact_header->doc_count_ptr < impact_header->doc_count_trim_ptr)
 	{
 	tf = *impact_header->impact_value_ptr;
 	tf = max(tf - g * pow(tf, g), 0);
-																										
+
 //	rsv = query_occurences * log((tf * collection_length_in_terms) / (u * cf) + 1.0);
 	rsv = query_occurences * ANT_logsum(log(tf) + log(collection_length_in_terms) - log(u * cf + 1.0), 0);
 
@@ -149,43 +122,6 @@ while (impact_header->doc_count_ptr < impact_header->doc_count_trim_ptr)
 void ANT_ranking_function_puurula::relevance_rank_top_k(ANT_search_engine_result *accumulator, ANT_search_engine_btree_leaf *term_details, ANT_compressable_integer *impact_ordering, long long trim_point, double prescalar, double postscalar, double query_frequency)
 {
 exit(printf("Cannot use this ranking function with this query processing strategy\n"));
-/*
-	long long docid;
-	double rsv, tf, cf, query_length, query_occurences, prior;
-	ANT_compressable_integer *current, *end;
-
-	current = impact_ordering;
-	end = impact_ordering + (term_details->local_document_frequency >= trim_point ? trim_point : term_details->local_document_frequency);
-
-	query_length = accumulator->get_term_count();
-	query_occurences = query_frequency;
-
-	cf = (double)term_details->global_collection_frequency;
-
-	while (current < end)
-		{
-		end += 2;		// account for the impact_order and the terminator
-		tf = *current++;
-		tf = max(tf - g * pow(tf, g), 0);
-
-		rsv = query_occurences * log((tf * prescalar * collection_length_in_terms) / (u * cf) + 1.0);
-
-		docid = -1;
-		while (*current != 0)
-			{
-			docid += *current++;
-
-			if (accumulator->is_zero_rsv(docid))		// unseen before now so add the document prior
-				{
-		 		prior = log(1.0 - discounted_document_lengths[(size_t)docid] / ((double)document_lengths[(size_t)docid] + u));
-				accumulator->add_rsv(docid, quantize(postscalar * (query_length * prior + rsv), maximum_collection_rsv, minimum_collection_rsv));
-				}
-			else
-				accumulator->add_rsv(docid, quantize(postscalar * rsv, maximum_collection_rsv, minimum_collection_rsv));
-			}
-		current++;		// skip over the zero
-		}
-*/
 }
 #endif
 
