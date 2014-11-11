a1b1dcc80f8e97fb37d228cb2e7380ba685ca2f6
Blake Burgess <bburgess@cs.otago.ac.nz>
2014-11-11 13:17:46 +1300

    tidied up simple8b

--- source/compress_simple8b.h
+++ source/compress_simple8b.h
@@ -14,16 +14,16 @@
 class ANT_compress_simple8b : public ANT_compress
 {
 protected:
-	static const long bits_to_use[];
-	static const unsigned char bits_used_table[];
-	static const unsigned char ints_packed_table[];
+	static long ints_packed_table[];
+	static long can_pack_table[];
+	static long row_for_bits_needed[];
+	static long invalid_masks_for_offset[];
+	static long simple8b_shift_table[];
 
 public:
 	ANT_compress_simple8b() {}
 	virtual ~ANT_compress_simple8b() {}
 
-	virtual void pack(ANT_compressable_integer *source, uint64_t *dest, uint32_t mask_type, uint32_t num_to_pack);
-	virtual long can_pack(ANT_compressable_integer *from, uint32_t mask_type, uint32_t pack_limit);
 	virtual long long compress(unsigned char *destination, long long destination_length, ANT_compressable_integer *source, long long source_integers);
 	virtual void decompress(ANT_compressable_integer *destination, unsigned char *source, long long destination_integers);
 } ;
