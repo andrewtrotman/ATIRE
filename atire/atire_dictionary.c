c970cd6e3dad816fd83d691e7a7be1c553e8208d
Blake Burgess <bburgess@cs.otago.ac.nz>
2014-11-13 16:36:38 +1300

    fixed whitespace for previous commit

--- atire/atire_dictionary.c
+++ atire/atire_dictionary.c
@@ -240,9 +240,9 @@ for (term = iterator.first(first_term); term != NULL; term = iterator.next())
 		printf("%s ", term);
 #endif
 #ifdef SPECIAL_COMPRESSION
-    if (leaf.local_document_frequency < 3)
-		  printf("%lld %lld 0", leaf.local_collection_frequency, leaf.local_document_frequency);
-    else
+		if (leaf.local_document_frequency < 3)
+			printf("%lld %lld 0", leaf.local_collection_frequency, leaf.local_document_frequency);
+		else
 #endif
 		printf("%lld %lld %lld", leaf.local_collection_frequency, leaf.local_document_frequency, leaf.postings_length);
 		if (check_postings && *term != '~')		// ~length and others aren't encoded in the usual way
