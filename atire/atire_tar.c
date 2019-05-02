b3c6c91c8ca55a4a790b2e5a3a9f29c91320c99f
Andrew Trotman <andrew@cs.otago.ac.nz>
2019-05-02 13:35:37 +1200

    trying to debug tar

--- atire/atire_tar.c
+++ atire/atire_tar.c
@@ -9,6 +9,7 @@
 #include "instream_deflate.h"
 #include "instream_buffer.h"
 #include "directory_iterator_tar.h"
+#include "directory_iterator_multiple.h"
 
 /*
 	MAIN()
@@ -21,13 +22,23 @@ ANT_instream *file_stream, *decompressor, *instream_buffer;
 ANT_directory_iterator *source;
 ANT_directory_iterator_object raw_file, *got;
 
-file_stream = new ANT_instream_file(&memory, argv[1]);
-decompressor = new ANT_instream_deflate(&memory, file_stream);
-instream_buffer = new ANT_instream_buffer(&memory, decompressor);
-source = new ANT_directory_iterator_tar(instream_buffer, ANT_directory_iterator::READ_FILE, ANT_directory_iterator_tar::NAME);
+ANT_directory_iterator_multiple *parallel_disk = new ANT_directory_iterator_multiple;
 
-for (got = source->first(&raw_file); got != NULL; got = source->next(&raw_file))
-	puts(raw_file.filename);
+for (int file = 1; file < argc; file++)
+	{
+	file_stream = new ANT_instream_file(&memory, argv[file]);
+	decompressor = new ANT_instream_deflate(&memory, file_stream);
+	instream_buffer = new ANT_instream_buffer(&memory, decompressor);
+	source = new ANT_directory_iterator_tar(instream_buffer, ANT_directory_iterator::READ_FILE, ANT_directory_iterator_tar::READ_FILE);
+	
+	parallel_disk->add_iterator(source);
+	}
+
+for (got = parallel_disk->first(&raw_file); got != NULL; got = parallel_disk->next(&raw_file))
+	{
+	printf("### %s\n", raw_file.filename);
+	puts(raw_file.file);
+	}
 
 return 0;
 }
